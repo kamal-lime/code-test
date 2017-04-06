543d7ad0aba79528d2e5ca9f77b99ef36a3bbc52
kamal-lime <kamalk@limeroad.com>
2017-04-06 17:35:48 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,6 @@
-cd /tmp/
-wget https://s3-ap-southeast-1.amazonaws.com/koovs-java-test/index.html
-sudo mv /tmp/index.html /var/www/html/
+#cd /tmp/
+#wget https://s3-ap-southeast-1.amazonaws.com/koovs-java-test/index.html
+#sudo mv /tmp/index.html /var/www/html/
+
+cd /mnt
+git clone https://github.com/kamal-lime/aws-cli.git
